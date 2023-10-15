cb8c1a875b302d7a77f6b8be406b49e0d29e56cb
NPC-Roman <128088564+NPC-Roman@users.noreply.github.com>
2023-10-15 13:35:01 +0300

    change05

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,38 @@ import Contacts from './Contacts/Contacts';
 import Filter from './Filter/Filter';
 import Form from './Form';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-1', name: 'Homer Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
+  };
+
+  componentDidMount() {
+    this.allContactsFromLS();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  allContactsFromLS = () => {
+    try {
+      return this.setState({
+        contacts: JSON.parse(localStorage.getItem('contacts')) ?? [],
+      });
+    } catch (error) {
+      return [];
+    }
   };
 
   onAddContact = newName => {
@@ -49,13 +72,14 @@ class App extends Component {
   };
 
   render() {
+    const { filter, contacts } = this.state;
     return (
       <>
         <h1
           style={{
             marginTop: '25px',
             textAlign: 'center',
-            color: '#000000',
+            color: 'rgb(145, 122, 122)',
           }}
         >
           Phonebook
@@ -66,21 +90,27 @@ class App extends Component {
           style={{
             marginTop: '20px',
             textAlign: 'center',
-            color: '#000000',
+            color: 'rgb(145, 122, 122)',
           }}
         >
           Contacts:
         </h2>
-        <Filter
-          value={this.state.filter}
-          filterContacts={this.onFilterContact}
-        />
-        <Contacts
-          contacts={this.getContactList()}
-          onRemove={this.onRemoveContact}
-        />
+
+        {contacts.length !== 0 && (
+          <Filter value={filter} filterContacts={this.onFilterContact} />
+        )}
+
+        {contacts.length !== 0 && (
+          <Contacts
+            contacts={this.getContactList()}
+            onRemove={this.onRemoveContact}
+          />
+        )}
+
+        <ToastContainer />
       </>
     );
   }
 }
+
 export default App;
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,10 +1,16 @@
 import css from './Contacts.module.css';
+import { IoIosContact } from 'react-icons/io';
 
 const Contacts = ({ contacts, onRemove }) => {
   return (
     <ul className={css.contactList}>
       {contacts.map(({ id, name, number }) => (
         <li key={name} className={css.contactListItem}>
+          <IoIosContact
+            size="20px"
+            color="rgb(85, 75, 75)"
+            style={{ marginRight: '5px' }}
+          />
           {name}: {number}{' '}
           <button
             type="button"
